84e229bab21d7542d457083ac31381700db4b56f
Liticiya <reshetuyak@gmail.com>
2024-10-20 14:50:02 +0700

    Change text

--- src/main/java/com/eltex/Main.java
+++ src/main/java/com/eltex/Main.java
@@ -2,6 +2,6 @@ package com.eltex;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("First App!");
     }
 }
